d8a4e2f32d2029045e5dbc01ae9c6db30b826c4e
johnny <johnnyyuri56@gmail.com>
2024-09-10 08:37:34 -0300

    correção bug IsValidCpf

--- hexagonal/src/main/java/com/johnny/hexagonal/adapters/out/repository/entity/CustomerEntity.java
+++ hexagonal/src/main/java/com/johnny/hexagonal/adapters/out/repository/entity/CustomerEntity.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collation = "customers")
+@Document(collection = "customers")
 public class CustomerEntity {
 
     @Id
@@ -13,6 +13,6 @@ public class CustomerEntity {
     private String name;
     private String cpf;
     private AddressEntity address;
-    private boolean isValidCpf;
+    private Boolean isValidCpf;
 
 }
